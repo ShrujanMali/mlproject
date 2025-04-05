19329e328a4a4c50da8b516a4f5255c9c0fe1de9
shrujanmali24@gmail.com <shrujanmali24@gmail.com>
2025-04-05 19:35:26 +0530

    Deployement Configuration AWS

--- src/utils.py
+++ src/utils.py
@@ -1,12 +1,12 @@
 import os
 import sys
-import numpy as np
-import dill
-import pandas as pd
-import pickle
-from sklearn.model_selection import GridSearchCV
 
+import numpy as np 
+import pandas as pd
+import dill
+import pickle
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
